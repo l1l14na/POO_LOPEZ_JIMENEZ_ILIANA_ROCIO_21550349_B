cc578793fe276736e2eb7c793b0a83af6efd44ae
l1l14na <99371983+l1l14na@users.noreply.github.com>
2022-02-19 16:15:47 -0700

    Add files via upload

---
+++ EVA1_1_HOLAMUNDO.java
@@ -0,0 +1,19 @@
+package poo;
+public class EVA1_1_HOLAMUNDO {//CUERPO DE UNA CLASE
+	/*
+	 PPALABRAS EN ROJO: palabras reservadas
+	 PALABRAS EN NEGRO: IDENTIFICADORES -> variables, constantes, clases, objrtos, funciones, etc.
+	 []: ARREGLO
+	 {}: DELIMITADORES 
+	 */
+	public static void main(String[] args) {//FUNCION
+		// TODO Auto-generated method stub
+		System.out.println("Hello word");
+
+	}
+
+}
+/*
+ 
+ */
+ 
