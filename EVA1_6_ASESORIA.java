8f0c015757daaa829020b626e92d19782e77c82d
l1l14na <99371983+l1l14na@users.noreply.github.com>
2022-03-02 10:05:15 -0700

    Add files via upload

---
+++ EVA1_6_ASESORIA.java
@@ -0,0 +1,21 @@
+package poo;
+
+public class EVA1_6_ASESORIA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int valor = Integer.MAX_VALUE;
+		System.out.println("Valor INT MAX = " +valor);
+		valor = valor + 1;
+		int valorM = Integer.MIN_VALUE;
+		System.out.println("Valor INT MIN= " +valorM);
+		valorM = valorM + 1;
+		System.out.println("ValorMax + 1 = " +valor);
+		short valor1 = Short.MAX_VALUE;
+		System.out.println("Valor SHORT = " +valor1);
+		short valorMin = Short.MIN_VALUE;
+		System.out.println("Valor SHORT = " +valorMin);
+		
+	}
+
+}
---
+++ EVA1_7_MEMORIA_1.java
@@ -0,0 +1,22 @@
+package poo;
+
+public class EVA1_7_MEMORIA_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int valor=5;
+		String cade = "Hola";
+		Prueba obj = new Prueba();
+		
+		System.out.println("Valor = "+valor);
+		System.out.println("Cadena = "+cade);
+		System.out.println("Objeto = "+obj);
+
+	}
+
+}
+class Prueba{
+	public void Saludar() {
+		System.out.println("Hola");
+	}
+}
